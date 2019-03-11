aab302116f604e6886cd2dae94d6f18c9ec22f6d
jinlisoft1 <huyang1997@yeah.net>
2019-03-11 14:06:45 +0800

    locationBluepoint

--- app/src/main/java/com/example/crud/AmapActivity.kt
+++ app/src/main/java/com/example/crud/AmapActivity.kt
@@ -2,16 +2,31 @@ package com.example.crud
 
 import android.app.Activity
 import android.os.Bundle
+import com.amap.api.maps2d.AMap
 import com.amap.api.maps2d.MapView
+import com.amap.api.maps2d.model.MyLocationStyle
+import kotlinx.android.synthetic.main.activity_map.*
+import kotlinx.android.synthetic.main.activity_map.view.*
 
 class AmapActivity : Activity() {
     internal var mMapView : MapView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
         mMapView = findViewById(R.id.map) as MapView
         mMapView!!.onCreate(savedInstanceState)
+        var aMap : AMap? = null
+        if(aMap == null){
+            aMap = map.map
+        }
+        var myLocationStyle = MyLocationStyle()
+        myLocationStyle.interval(2000)
+        aMap!!.setMyLocationStyle(myLocationStyle)
+        aMap.isMyLocationEnabled = true
+
+
 
     }
 
