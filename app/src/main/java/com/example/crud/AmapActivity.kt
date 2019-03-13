357464e4026be979ef8ca3f3d76ed6b110bd7308
jinlisoft1 <huyang1997@yeah.net>
2019-03-13 14:44:25 +0800

    location

--- app/src/main/java/com/example/crud/AmapActivity.kt
+++ app/src/main/java/com/example/crud/AmapActivity.kt
@@ -2,13 +2,20 @@ package com.example.crud
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import com.amap.api.location.AMapLocation
+import com.amap.api.location.AMapLocationClient
+import com.amap.api.location.AMapLocationClientOption
+import com.amap.api.location.AMapLocationListener
 import com.amap.api.maps2d.AMap
 import com.amap.api.maps2d.MapView
 import com.amap.api.maps2d.model.MyLocationStyle
 import kotlinx.android.synthetic.main.activity_map.*
 import kotlinx.android.synthetic.main.activity_map.view.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AmapActivity : Activity() {
+class AmapActivity : Activity() ,AMapLocationListener{
     internal var mMapView : MapView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +32,7 @@ class AmapActivity : Activity() {
         myLocationStyle.interval(2000)
         aMap!!.setMyLocationStyle(myLocationStyle)
         aMap.isMyLocationEnabled = true
+        location()
 
 
     }
@@ -48,4 +56,36 @@ class AmapActivity : Activity() {
         super.onSaveInstanceState(outState)
         mMapView!!.onSaveInstanceState(outState)
     }
+
+    override fun onLocationChanged(p0: AMapLocation?) {
+        if(p0 != null){
+            if(p0.errorCode == 0){
+                p0.locationType
+                p0.latitude
+                p0.longitude
+                p0.accuracy
+                var df = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                var date = Date(p0.time)
+                df.format(date)
+            }else{
+                Log.e("AmapError","locationeError, ErrorCode: ${p0.errorCode} ${p0.errorInfo}"
+                )
+            }
+        }
+    }
+
+    private fun location(){
+        var mLocationClient = AMapLocationClient(this)
+        var mLocationOption = AMapLocationClientOption()
+
+        mLocationClient.setLocationListener(this)
+
+        mLocationOption!!.setLocationMode(AMapLocationClientOption.AMapLocationMode.Device_Sensors)
+        mLocationOption.setInterval(2000)
+        mLocationClient.setLocationOption(mLocationOption)
+        mLocationClient.startLocation()
+
+    }
+
+
 }
--- app/src/main/java/com/example/crud/FirstActivity.kt
+++ app/src/main/java/com/example/crud/FirstActivity.kt
@@ -1,10 +1,19 @@
 package com.example.crud
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity
+import android.text.format.Time
+import android.util.Log
+import android.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_first.*
+import java.util.*
+import kotlin.math.log
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +30,32 @@ class FirstActivity : AppCompatActivity() {
         push_config.setOnClickListener{
             startActivity(Intent(this,PushSetMain::class.java))
         }
+
+        setSupportActionBar(my_toolbar)
+
+    }
+
+    override fun onStop() {
+
+        super.onStop()
+        var hello_noti = NotificationCompat.Builder(this,"CURD")
+            .setSmallIcon(R.drawable.notification_template_icon_low_bg)
+            .setContentTitle("Hi")
+            .setContentText("you have exit app ")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setBadgeIconType(NotificationCompat.BADGE_ICON_LARGE)
+
+        var mChannel = NotificationChannel("CURD","world",NotificationManager.IMPORTANCE_DEFAULT).apply {
+            setShowBadge(true)
+        }
+
+        var mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        mNotificationManager.createNotificationChannel(mChannel)
+
+        mNotificationManager.notify(11,hello_noti.build())
+        Log.d("com.jinlisoft","OnDestroy")
+
+
     }
 
 }
